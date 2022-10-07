d57de3c29d9f25febb4ff41bb8e179e257eda93c
Yaroslav Oliinyk <4106677@gmail.com>
2022-10-07 12:41:23 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let interval = null;
+// startBtn.disabled = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', onClickStartBtn);
+stoptBtn.addEventListener('click', onClickStopBtn);
+
+function onClickStartBtn() {
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    console.log('Interval HEX Start!');
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function onClickStopBtn() {
+  clearInterval(interval);
+  console.log('Interval HEX Stop!');
+  startBtn.disabled = false;
+}
